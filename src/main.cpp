6ef35d10d8de9126a2e7da88b721b024f66c484a
Ivan <23701564+IvanProgramming@users.noreply.github.com>
2023-06-13 15:44:06 +0300

    Added multithreading for http server

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,78 @@
-#include "main.h"
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/system/error_code.hpp>
 #include "router.h"
+
+using boost::asio::ip::tcp;
 
 bool shouldExit = false;
 
 /*
-	Signal handler for SIGINT (Ctrl+C)
-	@param signal The signal that was received
+    Signal handler for SIGINT (Ctrl+C)
+    @param signal The signal that was received
 */
-void handleSignal(int signal) {
-	if (signal == SIGINT) {
-		shouldExit = true;
-	}
+void handleSignal(const boost::system::error_code& error, int signal) {
+    if (!error && signal == SIGINT) {
+        shouldExit = true;
+    }
 }
 
+// Start accepting incoming connections
+void startAccept(boost::asio::ip::tcp::acceptor& acceptor, boost::asio::io_context& context)
+{
+    // Create a new socket for each accepted connection
+    auto socket = std::make_shared<tcp::socket>(context);
+
+    acceptor.async_accept(*socket,
+        [&, socket](const boost::system::error_code& ec) {
+            if (!ec) {
+                router(*socket);
+            }
+            else {
+                // Handle the error or log it
+                std::cerr << "Error accepting connection: " << ec.message() << std::endl;
+            }
+
+            // Start accepting the next connection
+            startAccept(acceptor, context);
+        });
+}
 
 /*
-	Main function, where the program starts.
+    Main function, where the program starts.
 */
 int main() {
-	try {
-		boost::asio::io_context io_context;
-		// Getting port from environment variable
-		int port = 8080;
+    try {
+        boost::asio::io_context io_context;
+        // Getting port from environment variable
+        int port = 8080;
 
-		// Creating acceptor
-		tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
+        // Creating acceptor
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
 
-		std::cout << "Server started on port " << port << std::endl;
+        std::cout << "Server started on port " << port << std::endl;
 
-		// Register the signal handler for SIGINT (Ctrl+C)
-		std::signal(SIGINT, handleSignal);
+        // Register the signal handler for SIGINT (Ctrl+C)
+        boost::asio::signal_set signals(io_context, SIGINT);
+        signals.async_wait(boost::bind(handleSignal,
+            boost::asio::placeholders::error, boost::asio::placeholders::signal_number));
 
-		std::cout << "Press Ctrl+C to exit" << std::endl;
-		std::cout << "Method" << "\t\t" << "Path" << "\t\t" << "Parsed in" << "\t" << "Response in" << std::endl;
-		std::cout << "-----------------------------------------------------------------" << std::endl;
+        std::cout << "Press Ctrl+C to exit" << std::endl;
+        std::cout << "Method" << "\t\t" << "Path" << "\t\t" << "Parsed in" << "\t" << "Response in" << std::endl;
+        std::cout << "-----------------------------------------------------------------" << std::endl;
 
-		while (!shouldExit) {
-			// Wait for a new connection
-			tcp::socket socket(io_context);
-			acceptor.accept(socket);
+        tcp::socket socket(io_context);
+        startAccept(acceptor, io_context);
 
-			// Handle the request from the connected client
-			router(socket);
-
-			// Close the socket
-			socket.close();
-		}
-
-
-	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
-	}
-	return 0;
+        // Run the IO context in a loop until shouldExit is set to true
+        while (!shouldExit) {
+            io_context.run();
+            io_context.restart();
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
 }
