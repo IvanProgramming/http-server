e6dfb8c1f26bd3208934f3c5c2e67a458dee2282
Ivan <23701564+IvanProgramming@users.noreply.github.com>
2023-06-13 16:27:59 +0300

    Added tests for multiple requests

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,11 +1,50 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/thread.hpp>
 
 // Include the file containing the code to test
 #include "router.cpp"
 #include "request.cpp"
 #include "response.cpp"
 #include "mimetypes.cpp"
+
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/thread.hpp>
+
+void makeRequest(const std::string& host, const std::string& port, const std::string& target) {
+	try {
+		boost::asio::io_context io_context;
+
+		// Resolve the host name and port number
+		boost::asio::ip::tcp::resolver resolver(io_context);
+		boost::asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(host, port);
+
+		// Establish a connection
+		boost::asio::ip::tcp::socket socket(io_context);
+		boost::asio::connect(socket, endpoints);
+
+		// Prepare the HTTP request
+		boost::beast::http::request<boost::beast::http::empty_body> req(boost::beast::http::verb::get, target, 11);
+		req.set(boost::beast::http::field::host, host);
+		req.set(boost::beast::http::field::user_agent, "Boost HTTP Client");
+
+		// Send the HTTP request
+		boost::beast::http::write(socket, req);
+
+		// Receive and print the HTTP response
+		boost::beast::flat_buffer buffer;
+		boost::beast::http::response<boost::beast::http::dynamic_body> res;
+		boost::beast::http::read(socket, buffer, res);
+
+		std::cout << res << std::endl;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+}
 
 TEST_CASE("Request Test Case") {
 	std::string request = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: curl/7.55.1\r\nAccept: */*\r\n\r\n";
@@ -110,4 +149,19 @@ TEST_CASE("Check file extension cut") {
 	std::string fileName = "train_on_fire.mp3";
 	std::string ext = getFileExtension(fileName);
 	CHECK(ext == "mp3");
+}
+
+TEST_CASE("Check multiple connections at once") {
+	const std::string host = "127.0.0.1";
+	const std::string port = "8080";
+	const std::string target = "/train.jpg";
+
+	const int numRequests = 100;
+	boost::thread_group threads;
+
+	for (int i = 0; i < numRequests; ++i) {
+		threads.create_thread(boost::bind(makeRequest, host, port, target));
+	}
+
+	threads.join_all();
 }
